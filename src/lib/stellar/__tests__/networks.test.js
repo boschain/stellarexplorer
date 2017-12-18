075f2036203a4c43a94f94ca1a6fe0dbb3933fcb
boschain <34636640+boschain@users.noreply.github.com>
2017-12-17 22:40:03 -0500

    Update networks.test.js

--- src/lib/stellar/__tests__/networks.test.js
+++ src/lib/stellar/__tests__/networks.test.js
@@ -3,10 +3,10 @@ import networks, {hostnameToNetwork} from '../networks'
 describe('hostnameToNetwork', () => {
   it('detects network correctly from the hostname', () => {
     // public network
-    expect(hostnameToNetwork('steexp.com')).toEqual(networks.public)
+    expect(hostnameToNetwork('boschain.org')).toEqual(networks.public)
 
     // test network
-    expect(hostnameToNetwork('testnet.steexp.com')).toEqual(networks.test)
+    expect(hostnameToNetwork('testnet.boschain.org')).toEqual(networks.test)
 
     // localhost for development
     expect(hostnameToNetwork('localnet.local')).toEqual(networks.local)
@@ -18,6 +18,6 @@ describe('hostnameToNetwork', () => {
     expect(hostnameToNetwork('')).toEqual(networks.local)
     expect(hostnameToNetwork('localhost')).toEqual(networks.local)
     expect(hostnameToNetwork('0.0.0.0')).toEqual(networks.local)
-    expect(hostnameToNetwork('not.steexp.com')).toEqual(networks.local)
+    expect(hostnameToNetwork('not.boschain.org')).toEqual(networks.local)
   })
 })
